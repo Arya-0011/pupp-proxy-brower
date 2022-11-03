5db9948a0ee4fa0e743de79b7144efe40561c015
Arya-0011 <aryaaniket2001@gmail.com>
2022-11-03 17:38:19 +0530

    rm -lock

--- app.js
+++ app.js
@@ -1,48 +1,69 @@
 const puppeteer = require('puppeteer');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-// starting Puppeteer
+const sleep = async function (seconds) {
+    return new Promise(resolve => setTimeout(resolve, seconds / 1000));
+}
 
-let retry = 0;
-let maxRetries = 5;
+// Function to Open the Proxy list page
+const getProxyList = async function (params) {
+    params.url = params.url || '';
 
-(async function scrape() {
-    const sleep = async function(time) {
-        return new Promise(resolve => setTimeout(resolve, time));
-    }
-    
-    retry++;
-
-    let proxyList = [
-        '217.150.210.20:7497',
-        '70.40.254.162:8111',
-        '24.252.24.5:8111',
-        '197.248.135.115:8111',
-        
-    ];
-
-    var proxy = proxyList[Math.floor(Math.random() * proxyList.length)];
-
-    console.log('proxy: ' + proxy);
+    if (params.url == '')
+        return 'Blank URLs not allowed!';
 
     const browser = await puppeteer.launch({
-        headless: false,
-        args: ['--proxy-server=socks5://' + proxy]
+        headless: params.headless || false,
+    });
+    const page = await browser.newPage();
+    await page.goto(params.url);
+
+    var ipData = {};
+
+    if (params.url.includes('scrapingant.com')) {
+        await page.waitForSelector('#proxies-list-loader > table > tbody > tr:nth-child(2)');
+        ipData.ip_address = await page.$$eval("#proxies-list-loader > table > tbody > tr:nth-child(2) > td:nth-child(1)", (nodes) => nodes.map((n) => n.innerText)[0]);
+        ipData.ip_address_port = await page.$$eval("#proxies-list-loader > table > tbody > tr:nth-child(2) > td:nth-child(2)", (nodes) => nodes.map((n) => n.innerText)[0]);
+        ipData.connection_type = await page.$$eval("#proxies-list-loader > table > tbody > tr:nth-child(2) > td:nth-child(3)", (nodes) => nodes.map((n) => n.innerText)[0]);
+    }
+   
+    console.log(ipData.ip_address)
+
+    await browser.close();
+
+    return ipData;
+};
+
+const openPageWithProxy = async function (params) {
+    const proxyDetails = await getProxyList({ url: 'https://scrapingant.com/free-proxies/', headless: params.headless });
+
+    const browser = await puppeteer.launch({
+        headless: params.headless,
+        args: ['--proxy-server=' + ((proxyDetails.connection_type == 'SOCKS5') ? 'socks5://' : '') + proxyDetails.ip_address + ':' + proxyDetails.ip_address_port]
     });
 
     try {
         const page = await browser.newPage();
-        // page.goto('https://www.google.com/search?q=what+is+my+ip');
-        await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4298.0 Safari/537.36');
+        page.goto('https://api.ipify.org?format=json');
     } catch (e) {
-
-
-        if (retry < maxRetries) {
-            scrape();
-        }
+        console.log(e)
     }
 
-    await sleep(100000000);
+    await sleep(50);
+    console.log(proxyDetails);
+    return proxyDetails;
+};
 
-    await browser.close();
 
-})();
+app.get('/openPageWithProxy', async (req, res) => {
+    const output = await openPageWithProxy({ headless: false });
+
+    res.send(JSON.stringify(output));
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
+

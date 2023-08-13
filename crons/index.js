4bd77568a555c764e6fba4f6b346c5a19cce8269
Arya0011 <aryaaniket2001@gmai.com>
2023-08-14 01:31:15 +0530

    puppeteer added

--- crons/index.js
+++
@@ -1,7 +0,0 @@
-const { checkOpenPRs } = require("./github/emailPendingPRs");
-const { deleteOldBranches } = require('./github/deleteStaleBranches')
-require('dotenv').config()
-
-checkOpenPRs(process.env.TOKEN, process.env.ORGANIZATION, "arya.aniket@tyreplex.com", ["arya.aniket@tyreplex.com", "test.tyreplex.com"]);
-
-deleteOldBranches(process.env.TOKEN, process.env.ORGANIZATION)
